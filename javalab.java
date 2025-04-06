14fcf1f789faa2d90669e08d9ce84481ca4ce52d
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:39:12 +0700

    4รับตัวอักษรจากผู้ใช้แล้วเปลี่ยนเป็นตัวพิมพ์ใหญ่หรือตัวพิมพ์เล็ก

--- javalab.java
+++ javalab.java
@@ -1,27 +1,10 @@
 import java.util.Scanner;
 
 public class javalab {
-    public static boolean isPalindrome(String str) {
-        int left = 0, right = str.length() - 1;
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
+    static Scanner s = new Scanner(System.in);
+    static String str = s.nextLine();
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("ป้อนคำที่ต้องการตรวจสอบ: ");
-        String input = scanner.nextLine();
-
-        if (isPalindrome(input)) {
-            System.out.println(input + " เป็น Palindrome");
-        } else {
-            System.out.println(input + " ไม่ใช่ Palindrome");
-        }
+        System.out.println(str.toUpperCase() + "\n" + str.toLowerCase());
     }
 }
