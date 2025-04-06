a8c82d522582deb5b1814fc33f05649ca7c9b6f6
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:30:08 +0700

    7แปลงอุณหภูมิจากองศาเซลเซียสเป็นองศาฟาเรนไฮต์ โดยใช้สูตร F = (C × 9/5) + 32

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,13 @@
+
 import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-                 double numA, numB, numC, average;
+                double celsius,fahrenheit;
 Scanner scanner = new Scanner(System.in);
-                System.out.print("Enter first number: ");
-               numA = scanner.nextDouble();
-                System.out.print("Enter second number: ");
-          numB = scanner.nextDouble();
-                System.out.print("Enter third number: ");
-               numC = scanner.nextDouble();
-              average = (numA + numB + numC) / 3;
-                System.out.println("Average: " + average);
+                System.out.print("Enter temperature in Celsius: ");
+                celsius = scanner.nextDouble();
+                fahrenheit = (celsius * 9/5) + 32;
+                System.out.println("Temperature in Fahrenheit: " + fahrenheit);
     }
 }
