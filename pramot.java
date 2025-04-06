c3f71b081d9a76f01d79da904002a89aced3022e
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:25:11 +0700

    4   เขียนโปรแกรมตรวจสอบเลขคู่หรือเลขคี่

--- pramot.java
+++ pramot.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
 
-public class pramot
- {
+public class pramot {
     public static void main(String[] args) {
-              int num1, num1; 
- Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-       num1 = scanner.nextInt();
-        System.out.print("Enter second number: ");
-        num2 = scanner.nextInt();
-        System.out.println("Sum: " + (num1 + num2));
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter a number: ");
+                int num = scanner.nextInt();
+                System.out.println((num % 2 == 0) ? "Even" : "Odd");
 }
 }
