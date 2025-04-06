827f09f57bb588f6e37e950a14e0c53851071a8e
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:04:08 +0700

    รับค่าชื่อจากผู้ใช้ และแสดงผลลัพธ์ว่า "Hello, [ชื่อ]"

---
+++ pramot.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class pramot {
+    public static void main(String[] args) {
+        String name;  
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();  
+        System.out.println("Hello, " + name);  
+}
+}
