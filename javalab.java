3c6f599a1cb4baf646604baf49f81c61cf554dc5
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:48:54 +0700

    9แปลงเลขฐาน 10 เป็นเลขฐาน 2 (Binary)

--- javalab.java
+++ javalab.java
@@ -1,13 +1,11 @@
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class javalab {
     static Scanner s = new Scanner(System.in);
-    static int n = s.nextInt(), i, numbers[] = new int[n];
+    static int num = s.nextInt();
 
     public static void main(String[] args) {
-        for (i = 0; i < n; i++) numbers[i] = s.nextInt();
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        System.out.println(Integer.toBinaryString(num));
     }
 }
