b8b4ae7fb203955a30bd7c5426c042df79b4d674
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:40:28 +0700

    5คำนวณเงินทอนโดยรับราคาสินค้าและจำนวนเงินที่จ่าย จากนั้นคืนเงินทอนเป็นแบงค์และเหรียญต่างๆ

--- javalab.java
+++ javalab.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class javalab {
     static Scanner s = new Scanner(System.in);
-    static String str = s.nextLine();
+    static int price = s.nextInt(), paid = s.nextInt(), change = paid - price, bills[] = {1000, 500, 100, 50, 20, 10, 5, 1};
 
     public static void main(String[] args) {
-        System.out.println(str.toUpperCase() + "\n" + str.toLowerCase());
+        for (int b : bills) if (change >= b) { System.out.println(b + " บาท: " + change / b); change %= b; }
     }
 }
