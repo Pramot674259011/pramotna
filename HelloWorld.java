fbbaacceeae0c638cb0bf13f1d59a44c9bff11e9
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:31:49 +0700

    9รับค่าปี ค.ศ. จากผู้ใช้ แล้วตรวจสอบว่าเป็นปีอธิกสุรทิน (Leap Year) หรือไม่

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-         double cm,inches;        
-Scanner scanner = new Scanner(System.in);
-                System.out.print("Enter length in cm: ");
-        cm = scanner.nextDouble();
-        inches = cm / 2.54;
-        System.out.println("Length in inches: " + inches);
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        System.out.println(isLeap ? "Leap Year" : "Not a Leap Year");
     }
 }
