d436fd7de954ff77f3f72d794ebd56c8a7af9bb1
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:18:49 +0700

    3รับค่าจำนวนเต็มสองจำนวนจากผู้ใช้ แล้วแสดงผลรวมของทั้งสองจำนวน

--- pramot.java
+++ pramot.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
-public class pramot {
+public class pramot
+ {
     public static void main(String[] args) {
-        String name;  
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        name = scanner.nextLine();  
-        System.out.println("Hello, " + name);  
+              int num1, num1; 
+ Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+       num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextInt();
+        System.out.println("Sum: " + (num1 + num2));
 }
 }
