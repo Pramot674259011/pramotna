389004feb17de183bf66c71b6dcdadee4a69d249
Pramot674259011 <674259011@webmail.npru.ac.th>
2025-04-06 20:32:23 +0700

    10คำนวณค่า Factorial ของตัวเลขที่รับเข้ามาโดยใช้ loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Enter a year: ");
-        int year = scanner.nextInt();
-        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-        System.out.println(isLeap ? "Leap Year" : "Not a Leap Year");
-    }
+                int factNum, factorial;l
+Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter a number: ");
+                int factNum = scanner.nextInt();
+                int factorial = 1;
+                for (int i = 1; i <= factNum; i++) {
+                    factorial *= i;
+                }
+                System.out.println("Factorial: " + factorial);
+        
+                scanner.close();
 }
+}    
